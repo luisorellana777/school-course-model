0df14e836e1c7a4efc956d84a4f7f148e7ef3186
Luis Orellana Altamirano <luis.orellana.a@usach.cl>
2021-06-23 22:27:26 -0400

    Initial Commit

---
+++ src/test/java/com/course/coursefirestore/CourseFirestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.coursefirestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseFirestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
